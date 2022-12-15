a4aa40bfa426910a1488d30e1b856a2cdc38b813
loc2k1 <101375249+loc2k1@users.noreply.github.com>
2022-12-15 22:51:52 +0700

    Add files via upload
    Signed-off-by: loc2k1 <101375249+loc2k1@users.noreply.github.com>

---
+++ wp-content/themes/manhphuc/thegioididong/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Flatsome functions and definitions
+ *
+ * @package flatsome
+ */
+
+require get_template_directory() . '/inc/init.php';
+wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/libs/font-awesome/css/font-awesome.min.css', array(), '4.7.0', 'all' );
+
+/**
+ * Note: It's not recommended to add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
+ * Learn more here: http://codex.wordpress.org/Child_Themes
+ */
+
